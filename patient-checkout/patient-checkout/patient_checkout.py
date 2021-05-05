efe1aa9680794bee93da503ddcc0f798e437b9b1
Debnath <debnsuma@3c22fb299a33.ant.amazon.com>
2021-05-05 14:17:29 +0530

    adding the patient bill demo

--- patient-checkout/patient-checkout/patient_checkout.py
+++ patient-checkout/patient-checkout/patient_checkout.py
@@ -2,8 +2,13 @@ import json
 import os 
 import boto3
 
+import logging 
+
 s3 = boto3.client('s3')
 sns = boto3.client('sns')
+logger = logging.getLogger("PatientCheckOut")
+logger.setLevel(logging.INFO)
+
 
 def lambda_handler(event, context):
     
@@ -12,6 +17,7 @@ def lambda_handler(event, context):
     # Fetch Bucket details from the event 
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_name = event['Records'][0]['s3']['object']['key']
+    logger.info(f"Reading file {file_name} from {bucket_name}")
     
     # Read the data from S3 
     obj = s3.get_object(Bucket=bucket_name, Key=file_name)
@@ -20,7 +26,8 @@ def lambda_handler(event, context):
 
     # Iterate through the all the data in the 'checlout_event'
     for each_event in checkout_events:
-        print(each_event)
+        logger.info("Message being published")
+        logger.info((each_event))
         sns.publish(
             TopicArn = topic,
             Message = json.dumps({"default" : json.dumps(each_event)}),
